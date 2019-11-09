735f53c2be0b315c3550cbf8eeab4c398b8694b3
Sporule <hao@sporule.com>
2019-11-09 21:56:59 +0000

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# rss-generator-webpack-plugin
+# Markdown RSS Generator Plugin
+
+> Generate RSS from markdown
+
+## Installation
+
+Node.js
+
+```bash
+npm install markdown-rss-generator-webpack-plugin
+```
+
+## Usage Example (ES6)
+
+In your Webpack Config file
+
+```javascript
+const RSSGeneratorPlugin = require("RSSGeneratorPlugin").default;
+
+//define the options
+const option = {
+                title: "Sporule",
+                outputPath: "rss.xml", //rss file output path
+                description: "Sporule is a micro blog site",
+                link: "https://www.sporule.com",
+                language: "en",
+                image: "https://i.imgur.com/vfh3Une.png",
+                favicon: "https://i.imgur.com/vfh3Une.png",
+                copyright: "All rights reserved 2019, Sporule",
+                updated: new Date(), //updated date
+                generator: "Sporule",
+                author: {
+                    name: "Sporule",
+                    email: "example@example.com",
+                    link: "https://www.sporule.com"
+                },
+                route: "/items"
+                //this is the route for the markdown link. If the path of the markdown files are /posts/sporule.md, and the page link for rendered markdown is /items/sporule, then the route is /items
+    }
+
+//In the plugin section
+
+ plugins: [
+  new RSSGeneratorPlugin(option)
+ ]
+
+```
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { RawSource } from "webpack-sources";
+import { Feed } from "feed";
+import MarkdownHandler from 'markdown-handler';
+
+
+export default class RSSGeneratorPlugin {
+    constructor(options = {}) {
+        this.options = Object.assign(
+            {
+                title: "Sporule",
+                outputPath: "rss.xml",
+                description: "Sporule is a micro blog site",
+                link: "https://www.sporule.com",
+                language: "en",
+                image: "https://i.imgur.com/vfh3Une.png",
+                favicon: "https://i.imgur.com/vfh3Une.png",
+                copyright: "All rights reserved 2019, Sporule",
+                updated: new Date(),
+                generator: "Sporule",
+                author: {
+                    name: "Sporule",
+                    email: "example@example.com",
+                    link: "https://www.sporule.com"
+                },
+                route: "/items"
+            },
+            options
+        );
+    }
+
+
+    apply(compiler) {
+        // Specify the event hook to attach to
+        compiler.hooks.emit.tapAsync(
+            'RSSGeneratorPlugin',
+            (compilation, callback) => {
+                let mds = [];
+                let mdHandler = new MarkdownHandler();
+                let regex = /.*\.md$/gm;
+                for (let path in compilation.assets) {
+                    if (path.search(regex) >= 0) {
+                        let md = compilation.assets[path].source().toString();
+                        mds.push(mdHandler.parseContent(path, md));
+                    }
+                }
+                let feed = new Feed({
+                    title: this.options.title,
+                    description: this.options.description,
+                    id: this.options.link + "/",
+                    link: this.options.link + "/",
+                    language: this.options.language,
+                    image: this.options.image,
+                    favicon: this.options.favicon,
+                    copyright: this.options.copyright,
+                    updated: this.options.updated,
+                    generator: this.options.generator,
+                    author: this.options.author
+                });
+                mds.forEach(md => {
+                    feed.addItem({
+                        title: md.title,
+                        id: this.options.link + md.path.replace(".md", "").replace("posts", this.options.route),
+                        link: this.options.link + md.path.replace(".md", "").replace("posts", this.options.route),
+                        description: md.excerpt,
+                        content: md.excerpt,
+                        date: new Date(md.metas.date),
+                        image: this.options.link + md.metas.coverimage
+                    })
+                })
+                compilation.assets[this.options.outputPath] = new RawSource(feed.atom1());
+                callback();
+            }
+        );
+
+
+    }
+}
